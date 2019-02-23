88208a6b3ce2a0abdf376f49108dc426617777ec
Javier Diaz-Mejia <34211920+jdime@users.noreply.github.com>
2019-02-22 23:10:21 -0500

    Add files via upload

---
+++ bin/obtains_dge_from_10x_3_files_barcodes_genes_matrix.R
@@ -0,0 +1,145 @@
+####################################
+### Javier Diaz - javier.diazmejia@gmail.com
+### Script to transform the three files from 10X barcodes.tsv, genes.tsv and matrix.mtx into a text file with genes in rows and barcodes in columns
+####################################
+
+####################################
+### Required libraries
+####################################
+### 'optparse'   to handle one-line-commands
+### 'Seurat'     to run QC, differential gene expression and clustering analyses
+### 'dplyr'      needed by Seurat for data manupulation
+####################################
+
+suppressPackageStartupMessages(library(Seurat))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(optparse))
+
+####################################
+### Turning warnings off for the sake of a cleaner aoutput
+####################################
+oldw <- getOption("warn")
+options( warn = -1 )
+
+####################################
+### Get inputs from command line argumets
+####################################
+#
+option_list <- list(
+  make_option(c("-i", "--input"), default="NA",
+              help="The path/name to a 10X *directory* with barcodes.tsv, genes.tsv and matrix.mtx files"),
+#
+  make_option(c("-o", "--outdir"), default="NA",
+              help="A path/name for the results directory"),
+#
+  make_option(c("-p", "--prefix_outfiles"), default="NA",
+              help="A prefix for outfile names, e.g. your project ID")
+)
+opt <- parse_args(OptionParser(option_list=option_list))
+
+# Input          <- opt$input
+# Outdir         <- opt$outdir
+# PrefixOutfiles <- opt$prefix_outfiles
+# Tempdir        <- "~/temp" ## Using this for temporary storage of outfiles because sometimes long paths of outdirectories casuse R to leave outfiles unfinished
+# Input          <- "~/FULL_CELLRANGER_OUTPUT/output_from_cellranger_2.1.1/outs/filtered_gene_bc_matrices/GRCh38-1.2.0_premrna/"
+# Outdir         <- "~/temp/"
+# PrefixOutfiles <- "check"
+
+StartTimeOverall<-Sys.time()
+
+####################################
+### Check that mandatory parameters are not 'NA' (default)
+####################################
+
+ListMandatory<-list("input", "outdir", "prefix_outfiles")
+for (param in ListMandatory) {
+  if (length(grep('^NA$',opt[[param]], perl = T))) {
+    stop(paste("Parameter -", param, " can't be 'NA' (default). Use option -h for help.", sep = "", collapse = ""))
+  }
+}
+
+####################################
+### Create outdirs
+####################################
+CommandsToGetUserHomeDirectory<-("eval echo \"~$USER\"")
+UserHomeDirectory<-system(command = CommandsToGetUserHomeDirectory, input = NULL, wait = T, intern = T)
+#
+Outdir<-gsub("^~/",paste(c(UserHomeDirectory,"/"), sep = "", collapse = ""), Outdir)
+Tempdir<-gsub("^~/",paste(c(UserHomeDirectory,"/"), sep = "", collapse = ""), Tempdir)
+Outdir<-gsub("/$", "", Outdir)
+Tempdir<-gsub("/$", "", Outdir)
+#
+dir.create(file.path(Outdir, "DGE"), recursive = T)
+dir.create(file.path(Tempdir), showWarnings = F, recursive = T)
+
+####################################
+### Load data
+####################################
+print("Loading 10X infiles")
+input.matrix <- Read10X(data.dir = Input)
+dim(input.matrix)
+
+####################################
+### Create a Seurat object
+####################################
+
+seurat.object  <- CreateSeuratObject(raw.data = input.matrix)
+seurat.object
+
+####################################
+### Write DGE out
+####################################
+OutfileDGE<-paste(Tempdir,"/",PrefixOutfiles,".DGE.tsv", sep="")
+
+Headers<-paste("GENES",paste(colnames(seurat.object@data),sep="",collapse = "\t"),sep="\t",collapse = "\t")
+write.table(Headers,file = OutfileDGE, row.names = F, col.names = F, sep="\t", quote = F)
+write.table(x=seurat.object@data, file = OutfileDGE, row.names = T, col.names = F, sep="\t", quote = F, append = T)
+
+####################################
+### Report used options
+####################################
+OutfileOptionsUsed<-paste(Tempdir,"/",PrefixOutfiles,".DGE_UsedOptions.txt", sep="")
+TimeOfRun<-format(Sys.time(), "%a %b %d %Y %X")
+write(file = OutfileOptionsUsed, x=c(TimeOfRun,"\n","Options used:"))
+
+for (optionInput in option_list) {
+  write(file = OutfileOptionsUsed, x=(paste(optionInput@short_flag, optionInput@dest, opt[optionInput@dest], sep="\t", collapse="\t")),append = T)
+}
+
+####################################
+### Report time used
+####################################
+EndTimeOverall<-Sys.time()
+
+TookTimeOverall        <-format(difftime(EndTimeOverall,        StartTimeOverall,        units = "min"))
+
+OutfileCPUusage<-paste(Tempdir,"/",PrefixOutfiles,".DGE_CPUusage.tsv", sep="")
+ReportTime<-c(
+  paste("overall",TookTimeOverall,collapse = "\t")
+)
+
+write(file = OutfileCPUusage, x=c(ReportTime))
+
+####################################
+### Moving outfiles into outdir
+####################################
+outfiles_to_move <- list.files(Tempdir,pattern = paste(PrefixOutfiles, ".DGE_", sep=""), full.names = F)
+sapply(outfiles_to_move,FUN=function(eachFile){ 
+  file.copy(from=paste(Tempdir,"/",eachFile,sep=""),to=paste(Outdir,"/DGE/",eachFile,sep=""),overwrite=T)
+  file.remove(paste(Tempdir,"/",eachFile,sep=""))
+})
+
+####################################
+### Turning warnings on
+####################################
+options(warn = oldw)
+
+####################################
+### Finish
+####################################
+
+print("END - All done!!! Took time:")
+print(ReportTime)
+
+quit()
+
---
+++ bin/obtains_knee_plot_from_reads_per_barcode.R
@@ -0,0 +1,224 @@
+####################################
+### Javier Diaz - javier.diazmejia@gmail.com
+### Script to obtain either:
+### (a) read-counts from a possorted_genome_bam.bam file and then (b) a knee-plot and it's inflection, or
+### entering (a), will obtain (b)
+###
+### Note (a) is pressumably the output from Drop-seq_tools script 'BAMTagHistogram' (http://mccarrolllab.com/dropseq/)
+####################################
+
+####################################
+### Required libraries
+####################################
+### 'optparse'   to handle one-line-commands
+### 'dropbead'   to generate the knee-plot and get inflection. Install with:
+###              library(devtools)
+###              install_github("rajewsky-lab/dropbead")
+####################################
+
+####################################
+### Required external dependencies
+####################################
+### Dropseq-tools 'BAMTagHistogram' if using '-t BAM'
+####################################
+
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(dropbead))
+
+### Change this path as needed, if using '-t BAM'
+BAMTagHistogram <- "~/PROGRAMS/DROPSEQ/Drop-seq_tools-1.13/BAMTagHistogram"
+
+####################################
+### Get inputs from command line argumets
+####################################
+
+option_list <- list(
+  make_option(c("-i", "--infile"), default="NA",
+              help="A path/name to either a <tab> delimited *file* with number of reads per barcode, like:
+              #INPUT=/path_to_file/file.bam  TAG=XC FILTER_PCR...
+              203177  CTTATGGCTTTA
+              196369  TTAGCGCTTATA
+              151561  CGGCTGCTAATC
+              Presumably produced by Dropseq-tools 'BAMTagHistogram'
+              Or
+              A possorted_genome_bam.bam file resumably produced by 'cellranger count'
+              In the last case 'BAMTagHistogram' will be run first"),
+  #
+  make_option(c("-t", "--input_type"), default="NA",
+              help="Indicates if input is either a 'TAB' delimited file or a 'BAM' file"),
+  #   
+  make_option(c("-o", "--outdir"), default="NA",
+              help="A path/name for the results directory"),
+  #
+  make_option(c("-p", "--prefix_outfiles"), default="NA",
+              help="A prefix for outfile names, e.g. your project ID"),
+  #
+  make_option(c("-c", "--cluster_name"), default="NA",
+              help="Name of either of the following clusters where process are running
+              'mordor'
+              'h4h'
+              'samwise'
+              'local'")
+)
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+Infile          <- opt$infile
+InputType       <- opt$input_type
+Outdir          <- opt$outdir
+PrefixOutfiles  <- opt$prefix_outfiles
+ClusterName     <- opt$cluster_name
+Tempdir         <- "~/temp" ## Using this for temporary storage of outfiles because sometimes long paths of outdirectories casuse R to leave outfiles unfinished
+
+MaxNumberOfCells <- 30000 ### Note this parameter may change the inflection point
+
+# Infile          <- "~/FULL_CELLRANGER_OUTPUT/output_from_cellranger_2.1.1/outs/possorted_genome_bam.bam"
+# InputType       <- "BAM"
+# Outdir          <- "~/temp/"
+# PrefixOutfiles  <- "test"
+# ClusterName     <- "h4h"
+
+StartTimeOverall<-Sys.time()
+
+####################################
+### Check that mandatory parameters are not 'NA' (default)
+####################################
+
+ListMandatory<-list("infile", "input_type", "outdir", "prefix_outfiles", "cluster_name")
+for (param in ListMandatory) {
+  if (length(grep('^NA$',opt[[param]], perl = T))) {
+    stop(paste("Parameter -", param, " can't be 'NA' (default). Use option -h for help.", sep = "", collapse = ""))
+  }
+}
+
+####################################
+### Create outdirs and get path of files
+### Basically replacing any '~/' by the User HOME, because otherwise it conflicts with system(...)
+####################################
+writeLines("\n*** Create outdirs ***\n")
+CommandsToGetUserHomeDirectory<-("eval echo \"~$USER\"")
+UserHomeDirectory<-system(command = CommandsToGetUserHomeDirectory, input = NULL, wait = T, intern = T)
+#
+Outdir<-gsub("^~/",paste(c(UserHomeDirectory,"/"), sep = "", collapse = ""), Outdir)
+Tempdir<-gsub("^~/",paste(c(UserHomeDirectory,"/"), sep = "", collapse = ""), Tempdir)
+Outdir<-gsub("/$", "", Outdir)
+Tempdir<-gsub("/$", "", Outdir)
+#
+Infile<-gsub("^~/",paste(c(UserHomeDirectory,"/"), sep = "", collapse = ""), Infile)
+BAMTagHistogram<-gsub("^~/",paste(c(UserHomeDirectory,"/"), sep = "", collapse = ""), BAMTagHistogram)
+#
+dir.create(file.path(Outdir),   showWarnings = F, recursive = T)
+dir.create(file.path(Tempdir),  showWarnings = F, recursive = T)
+
+OutPdf<-paste(Tempdir,"/", PrefixOutfiles, ".kneeplot.pdf",collapse="",sep="")
+OutInf<-paste(Tempdir,"/", PrefixOutfiles, ".kneeplot_inflection.txt",collapse="",sep="")
+
+####################################
+### Obtain read-counts per cell
+####################################
+
+if(regexpr("^TAB$", InputType, ignore.case = T)[1] == 1) {
+  print("Loading *tab infile")
+  InfileTab <- Infile
+}else if (regexpr("^BAM$", InputType, ignore.case = T)[1] == 1) {
+  print("Processing possorted_genome_bam.bam infile")
+  print(Sys.time())
+
+  InfileTab <- paste(Tempdir, "/" , PrefixOutfiles, ".kneeplot_reads_per_barcode.tsv",collapse="",sep="")
+  OutfileCommandsForBAMTagHistogram<-paste(Tempdir,"/",PrefixOutfiles,".kneeplot_CommandsForBAMTagHistogram.sh", sep="")
+  write(file = OutfileCommandsForBAMTagHistogram, x=c("module load java/8",
+                                              paste(BAMTagHistogram, " \\" , sep = "", collapse = ""),
+                                              paste("I=", Infile, " \\", sep = "", collapse = ""),
+                                              paste("O=", InfileTab, " \\", sep = "", collapse = ""),
+                                              "TAG=CB"))
+  Chmod<-paste("chmod +x ", OutfileCommandsForBAMTagHistogram, sep = "", collapse = "")
+  system(command = Chmod[[1]],  input = NULL, intern = TRUE)
+  
+  ### Runnig BAMTagHistogram
+  ### Note in clusters we have to wait for the 'qsub' (PBS) process to finish
+  ### We'll monitor this by waiting until InfileTab is written
+    if (file.exists(InfileTab)) {
+    file.remove(InfileTab) ## will remove preexisting InfileTab (if any)
+    }
+
+    if(regexpr("^local$", ClusterName, ignore.case = T)[1] == 1) {
+      CommandToGetKneePlot<-OutfileCommandsForBAMTagHistogram
+    }else if (regexpr("^h4h$", ClusterName, ignore.case = T)[1] == 1) {
+      stop(paste("This script works with option '-c local' but it fails in clusters because it need to figure out how to pass a 'qsub' call"))
+      CommandToGetKneePlot<-paste("qsub -q all -l vmem=30G,walltime=10:00:00 ", OutfileCommandsForBAMTagHistogram, sep = "", collapse = "")
+    }else if (regexpr("(^mordor$|^samwise$)", ClusterName, ignore.case = T)[1] == 1) {
+      stop(paste("This script works with option '-c local' but it fails in clusters because it need to figure out how to pass a 'qsub' call"))
+      CommandToGetKneePlot<-paste("qsub -q all.q -l vmem=30G,walltime=10:00:00 ", OutfileCommandsForBAMTagHistogram, sep = "", collapse = "")
+    }else{
+      stop(paste("Unexpected name of cluster: ", ClusterName, "\n\nFor help type:\n\nRscript obtains_knee_plot_from_reads_per_barcode.R -h\n\n", sep=""))
+    }
+ 
+ print(CommandToGetKneePlot)
+ system(command = CommandToGetKneePlot, input = NULL, intern = TRUE)
+
+    ### Wait for InfileTab to be written
+    while (!file.exists(InfileTab)) {
+      Sys.sleep(1)
+    }
+  print("Done - processing possorted_genome_bam.bam infile")
+  print(paste("Created file:", InfileTab), sep="", collapse="")
+  print(Sys.time())
+
+}else{
+ stop(paste("Unexpected type of infile: ", InputType, "\n\nFor help type:\n\nRscript obtains_knee_plot_from_reads_per_barcode.R -h\n\n", sep=""))
+}
+
+####################################
+### Load read-counts per cell data and get knee-plot
+####################################
+print(paste("Start reading table of read-counts from:",InfileTab))
+print(Sys.time())
+
+mat<-as.data.frame(read.table(InfileTab,header=F))
+pdf(OutPdf)
+plotCumulativeFractionOfReads(mat, cutoff = MaxNumberOfCells, draw.knee.point = TRUE)
+dev.off()
+InflectionPoint<-estimateCellNumber(mat[, 1], max.cells = MaxNumberOfCells)
+write(file=OutInf,x=InflectionPoint)
+
+####################################
+### Report used options
+####################################
+OutfileOptionsUsed<-paste(Tempdir,"/",PrefixOutfiles,".kneeplot_UsedOptions.txt", sep="")
+TimeOfRun<-format(Sys.time(), "%a %b %d %Y %X")
+write(file = OutfileOptionsUsed, x=c(TimeOfRun,"\n","Options used:"))
+
+for (optionInput in option_list) {
+  write(file = OutfileOptionsUsed, x=(paste(optionInput@short_flag, optionInput@dest, opt[optionInput@dest], sep="\t", collapse="\t")),append = T)
+}
+
+####################################
+### Report time used
+####################################
+EndTimeOverall<-Sys.time()
+
+TookTimeOverall <-format(difftime(EndTimeOverall, StartTimeOverall, units = "secs"))
+
+OutfileCPUusage<-paste(Tempdir,"/",PrefixOutfiles,".kneeplot_CPUusage.txt", sep="")
+ReportTime<-c(
+  paste("overall",TookTimeOverall,collapse = "\t")
+)
+
+write(file = OutfileCPUusage, x=c(ReportTime))
+
+####################################
+### Moving outfiles into outdir
+####################################
+
+outfiles_to_move <- list.files(Tempdir,pattern = c(paste(PrefixOutfiles, ".kneeplot", sep=""), paste(PrefixOutfiles, ".reads_per_barcode.tsv", sep="")), full.names = F)
+sapply(outfiles_to_move,FUN=function(eachFile){
+  file.copy(from=paste(Tempdir,"/",eachFile,sep=""),to=paste(Outdir,"/",eachFile,sep=""),overwrite=T)
+  file.remove(paste(Tempdir,"/",eachFile,sep=""))
+})
+
+####################################
+### Finish
+####################################
+
+print("END - All done!!!")
+quit()
