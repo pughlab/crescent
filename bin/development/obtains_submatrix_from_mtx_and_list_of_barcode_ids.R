301cedc2a573a16d4e4ac07a449afd820bbd0395
Javier Diaz-Mejia <34211920+jdime@users.noreply.github.com>
2021-06-20 22:02:38 -0400

    Update obtains_submatrix_from_mtx_and_list_of_barcode_ids.R
    -Changed use of SubsetData() by subset() to make this script compatible with Seurat v4.0
    -Various documentation changes

--- bin/development/obtains_submatrix_from_mtx_and_list_of_barcode_ids.R
+++ bin/development/obtains_submatrix_from_mtx_and_list_of_barcode_ids.R
@@ -7,10 +7,7 @@
 ### Required libraries
 ####################################
 suppressPackageStartupMessages(library(DropletUtils)) # (Bioconductor) to handle MTX/H5 format files. Note it has about the same speed than library(earlycross) which can't handle H5
-suppressPackageStartupMessages(library(Seurat))       # (CRAN) to run QC, differential gene expression and clustering analyses
-### Seurat v3 can be installed like:
-### install.packages('devtools')
-### devtools::install_github(repo = 'satijalab/seurat', ref = 'release/3.0')
+suppressPackageStartupMessages(library(Seurat))       # (CRAN) to subset data. Tested with v4.0.2
 suppressPackageStartupMessages(library(dplyr))        # (CRAN) needed by Seurat for data manupulation
 suppressPackageStartupMessages(library(optparse))     # (CRAN) to handle one-line-commands
 ####################################
@@ -20,6 +17,8 @@ suppressPackageStartupMessages(library(optparse))     # (CRAN) to handle one-lin
 ####################################
 oldw <- getOption("warn")
 options( warn = -1 )
+
+ThisScriptName <- "obtains_submatrix_from_mtx_and_list_of_barcode_ids.R"
 
 ####################################
 ### Get inputs from command line argumets
@@ -82,14 +81,13 @@ SelectGenes    <- opt$select_genes
 Outdir         <- opt$outdir
 PrefixOutfiles <- "selected_feature_bc_matrix"
 
-####################################
-### Start stopwatches
-####################################
-
-StopWatchStart <- list()
-StopWatchEnd   <- list()
-
-StopWatchStart$Overall  <- Sys.time()
+#####
+OneLineCommands <- paste0("\n", "One-line-commands used:", "\n", "`Rscript /path_to/", ThisScriptName)
+for (optionInput in option_list) {
+  OneLineCommands <- paste0(OneLineCommands, paste0(" ", optionInput@short_flag, " ", opt[optionInput@dest]))
+}
+OneLineCommands <- paste0(OneLineCommands, paste0("`\n"))
+writeLines(OneLineCommands)
 
 ####################################
 ### Check that mandatory parameters are not 'NA' (default)
@@ -103,25 +101,69 @@ for (param in ListMandatory) {
 }
 
 ####################################
-### Create outdirs
+### Start stopwatches
 ####################################
 
-## Using `Tempdir` for temporary storage of outfiles because sometimes long paths of outdirectories casuse R to leave outfiles unfinished
-## Then at the end of the script they'll be moved into `Outdir/ProgramOutdir`
-Tempdir        <- "~/temp" ## Using this for temporary storage of outfiles because sometimes long paths of outdirectories casuse R to leave outfiles unfinished
+StopWatchStart <- list()
+StopWatchEnd   <- list()
+
+StopWatchStart$Overall  <- Sys.time()
+
+####################################
+### Define outdirs and CWL parameters
+####################################
+writeLines("\n*** Create outdirs ***\n")
+
+FILE_TYPE_OUT_DIRECTORIES = c(
+  "LOG_FILES"
+)
+
+Tempdir           <- "~/temp"
+UserHomeDirectory <- Sys.getenv("HOME")[[1]]
 #
-CommandsToGetUserHomeDirectory<-("eval echo \"~$USER\"")
-UserHomeDirectory<-system(command = CommandsToGetUserHomeDirectory, input = NULL, wait = T, intern = T)
-#
-Outdir<-gsub("^~/",paste(c(UserHomeDirectory,"/"), sep = "", collapse = ""), Outdir)
-Tempdir<-gsub("^~/",paste(c(UserHomeDirectory,"/"), sep = "", collapse = ""), Tempdir)
+Outdir<-gsub("^~/",paste0(UserHomeDirectory,"/"), Outdir)
+Tempdir<-gsub("^~/",paste0(UserHomeDirectory,"/"), Tempdir)
 Outdir<-gsub("/+", "/", Outdir, perl = T)
 Tempdir<-gsub("/+", "/", Tempdir, perl = T)
 Outdir<-gsub("/$", "", Outdir)
 Tempdir<-gsub("/$", "", Tempdir)
 #
-OutdirFinal<-paste(Outdir, "/", PrefixOutfiles,  sep = "", collapse = "")
-dir.create(file.path(OutdirFinal), recursive = T)
+dir.create(file.path(Outdir), showWarnings = F, recursive = T)
+dir.create(file.path(Tempdir), showWarnings = F, recursive = T)
+
+sapply(FILE_TYPE_OUT_DIRECTORIES, FUN=function(eachdir) {
+  dir.create(file.path(paste0(Tempdir, "/", eachdir)), showWarnings = F, recursive = T)
+})
+
+####################################
+### Report used options
+####################################
+writeLines("\n*** Report used options ***\n")
+
+StopWatchStart$ReportUsedOptions  <- Sys.time()
+
+OutfileOptionsUsed<-paste0(Tempdir, "/LOG_FILES/", PrefixOutfiles, "_SubmatrixFromMtxAndListOfBarcodes_UsedOptions", ".txt")
+
+TimeOfRun<-format(Sys.time(), "%a %b %d %Y %X")
+write(file = OutfileOptionsUsed, x=paste0("Run started: ", TimeOfRun, "\n"))
+
+write(file = OutfileOptionsUsed, x=c("Commands used:"), append = T)
+for (optionInput in option_list) {
+  write(file = OutfileOptionsUsed, x=(paste(optionInput@short_flag, optionInput@dest, opt[optionInput@dest], sep="\t", collapse="\t")),append = T)
+}
+
+write(file = OutfileOptionsUsed, x = OneLineCommands)
+
+StopWatchEnd$ReportUsedOptions  <- Sys.time()
+
+####################################
+### Report R sessionInfo
+####################################
+writeLines("\n*** Report R sessionInfo ***\n")
+
+OutfileRSessionInfo<-paste0(Tempdir, "/LOG_FILES/", PrefixOutfiles, "_SubmatrixFromMtxAndListOfBarcodes_RSessionInfo", ".txt")
+writeLines(capture.output(sessionInfo()), OutfileRSessionInfo)
+capture.output(sessionInfo())
 
 ####################################
 ### Load MTX data
@@ -171,10 +213,8 @@ if ((grepl(pattern = "^[0-9]+$", x = SelectBarcodes)) == TRUE) {
     sampled.barcodes<-gsub(x = as.factor(sampled.barcodes[,1]), pattern = "-[0-9]+$", perl = T, replacement = "")
   }
   
-  seurat.object.subsampled.barcodes <- SubsetData(object = seurat.object.u, cells = as.vector(sampled.barcodes))
+  seurat.object.subsampled.barcodes <- subset(x = seurat.object.u, cells = as.vector(sampled.barcodes))
 }
-print("Seurat object with subsampled barcodes:")
-seurat.object.subsampled.barcodes
 
 StopWatchEnd$DetermineBarcodesToSubsampleAndSubsample  <- Sys.time()
 
@@ -207,41 +247,30 @@ writeLines("\n*** Write subsampled matrix ***\n")
 
 StopWatchStart$WriteSubsampledMatrix  <- Sys.time()
 
-write10xCounts(path = OutdirFinal, x = seurat.object.subsampled.barcodes.genes@assays[["RNA"]]@data, gene.type="Gene Expression", overwrite=T, type="sparse", version="3")
+OutdirFinal <- paste0(Outdir, "/", PrefixOutfiles)
+write10xCounts(path = OutdirFinal, x = seurat.object.subsampled.barcodes.genes@assays[["RNA"]]@data, gene.type="Gene Expression",
+               overwrite=T, type="sparse", version="3")
 
 StopWatchEnd$WriteSubsampledMatrix  <- Sys.time()
 
 ####################################
-### Report used options
-####################################
-writeLines("\n*** Report used options ***\n")
-
-OutfileOptionsUsed<-paste(Tempdir,"/",PrefixOutfiles,".Subsample_UsedOptions.txt", sep="")
-TimeOfRun<-format(Sys.time(), "%a %b %d %Y %X")
-write(file = OutfileOptionsUsed, x=c(TimeOfRun,"\n","Options used:"))
-
-for (optionInput in option_list) {
-  write(file = OutfileOptionsUsed, x=(paste(optionInput@short_flag, optionInput@dest, opt[optionInput@dest], sep="\t", collapse="\t")),append = T)
-}
-
-####################################
 ### Obtain computing time used
 ####################################
-writeLines("\n*** Obtain computing time used***\n")
+writeLines("\n*** Obtain computing time used ***\n")
 
 StopWatchEnd$Overall  <- Sys.time()
 
-OutfileCPUusage<-paste(Tempdir,"/",PrefixOutfiles,".Subsample_CPUusage.txt", sep="")
+OutfileCPUtimes<-paste0(Tempdir, "/LOG_FILES/", PrefixOutfiles, "_SubmatrixFromMtxAndListOfBarcodes_CPUtimes", ".txt")
 
 Headers<-paste("Step", "Time(minutes)", sep="\t")
-write.table(Headers,file = OutfileCPUusage, row.names = F, col.names = F, sep="\t", quote = F, append = T)
+write.table(Headers,file = OutfileCPUtimes, row.names = F, col.names = F, sep="\t", quote = F, append = T)
 
 for (stepToClock in names(StopWatchStart)) {
   TimeStart <- StopWatchStart[[stepToClock]]
   TimeEnd   <- StopWatchEnd[[stepToClock]]
   TimeDiff <- format(difftime(TimeEnd, TimeStart, units = "min"))
   ReportTime<-c(paste(stepToClock, TimeDiff, sep = "\t", collapse = ""))
-  write(file = OutfileCPUusage, x=gsub(pattern = " mins", replacement = "", x = ReportTime), append = T)
+  write(file = OutfileCPUtimes, x=gsub(pattern = " mins", replacement = "", x = ReportTime), append = T)
 }
 
 ####################################
@@ -250,7 +279,7 @@ for (stepToClock in names(StopWatchStart)) {
 writeLines("\n*** Moving outfiles into outdir ***\n")
 writeLines(paste(Outdir,"/", PrefixOutfiles, sep="",collapse = ""))
 
-outfiles_to_move <- list.files(Tempdir, pattern = paste(PrefixOutfiles, ".Subsample_", sep=""), full.names = F)
+outfiles_to_move <- list.files(Tempdir, pattern = paste(PrefixOutfiles, "_SubmatrixFromMtxAndListOfBarcodes_", sep=""), full.names = F)
 sapply(outfiles_to_move,FUN=function(eachFile){
   ### using two steps instead of just 'file.rename' to avoid issues with path to ~/temp in cluster systems
   if (Outdir == Tempdir) {
@@ -271,6 +300,6 @@ options(warn = oldw)
 ####################################
 ### Finish
 ####################################
-writeLines(paste("END - All done!!! See:\n", OutfileCPUusage, "\nfor computing times report", sep = "", collapse = ""))
+writeLines(paste("\nEND - All done!!! See:\n", Outdir, "/selected_feature_bc_matrix", "\nfor outfiles", sep = "", collapse = "\n\n"))
 
 quit()
